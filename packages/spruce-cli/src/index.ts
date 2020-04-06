47e67dfcf073b960418752becf4dc2eb3dbc1bc1
liquidg3 <me@taylorrome.ro>
2020-04-06 13:48:15 -0600

    cwd fix

--- packages/spruce-cli/src/index.ts
+++ packages/spruce-cli/src/index.ts
@@ -63,9 +63,9 @@ async function setup(argv: string[], debugging: boolean): Promise<void> {
 	})
 
 	// Starting cwd
-	// const cwd = process.cwd()
+	const cwd = process.cwd()
 	// Force run in schema for now
-	const cwd = '/Users/taylorromero/Development/SpruceLabs/spruce-schema/'
+	// const cwd = '/Users/taylorromero/Development/SpruceLabs/spruce-schema/'
 
 	// Setup log
 	log.setOptions({ level: LogLevel.Info })
