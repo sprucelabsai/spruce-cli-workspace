1b1fdba1c0890678231a39a593527397dd7765cd
liquidg3 <me@taylorrome.ro>
2020-04-09 10:54:14 -0600

    lint

--- packages/spruce-cli/src/stores/AbstractStore.ts
+++ packages/spruce-cli/src/stores/AbstractStore.ts
@@ -1,119 +1,119 @@
-import fs from "fs";
-import { Mercury } from "@sprucelabs/mercury";
-import { Log } from "@sprucelabs/log";
-import SpruceError from "../errors/SpruceError";
-import { ErrorCode } from "#spruce/errors/codes.types";
-import { IUtilities } from "../utilities";
+import fs from 'fs'
+import { Mercury } from '@sprucelabs/mercury'
+import { Log } from '@sprucelabs/log'
+import SpruceError from '../errors/SpruceError'
+import { ErrorCode } from '#spruce/errors/codes.types'
+import { IUtilities } from '../utilities'
 
 /** Are we running globally or locally? */
 export enum StoreScope {
-	Global = "global",
-	Local = "local"
+	Global = 'global',
+	Local = 'local'
 }
 
 /** Are we authed as a user or a skill? */
 export enum StoreAuth {
-	User = "user",
-	Skill = "skill"
+	User = 'user',
+	Skill = 'skill'
 }
 
 /** Options needed by the store on instantiation */
 export interface IStoreOptions {
-	utilities: IUtilities;
-	mercury: Mercury;
-	cwd: string;
-	scope?: StoreScope;
-	authType?: StoreAuth;
-	log: Log;
+	utilities: IUtilities
+	mercury: Mercury
+	cwd: string
+	scope?: StoreScope
+	authType?: StoreAuth
+	log: Log
 }
 
 export interface IBaseStoreSettings {
-	authType: StoreAuth;
+	authType: StoreAuth
 }
 
 export default abstract class AbstractStore<
 	Settings extends IBaseStoreSettings = IBaseStoreSettings
 > {
 	/** The current scope */
-	public scope = StoreScope.Global;
+	public scope = StoreScope.Global
 
 	/** For logging */
-	public log: Log;
+	public log: Log
 
 	/** All the utilities */
-	public utilities: IUtilities;
+	public utilities: IUtilities
 
 	/** How we're logged in, user or skill */
 	public get authType() {
-		return this.readValue("authType") ?? StoreAuth.User;
+		return this.readValue('authType') ?? StoreAuth.User
 	}
 
 	public set authType(type: StoreAuth) {
-		this.writeValue("authType", type);
+		this.writeValue('authType', type)
 	}
 
 	/** For making calls to the world */
-	public mercury: Mercury;
+	public mercury: Mercury
 
 	/** Current directory for all operations */
-	public cwd: string;
+	public cwd: string
 
 	/** A name each store must set */
-	abstract name: string;
+	abstract name: string
 
 	public constructor(options: IStoreOptions) {
-		const { mercury, cwd, scope, authType, log, utilities } = options;
+		const { mercury, cwd, scope, authType, log, utilities } = options
 
-		this.mercury = mercury;
-		this.cwd = cwd;
-		this.scope = scope ?? this.scope;
-		this.authType = authType ?? this.authType;
-		this.log = log;
-		this.utilities = utilities;
+		this.mercury = mercury
+		this.cwd = cwd
+		this.scope = scope ?? this.scope
+		this.authType = authType ?? this.authType
+		this.log = log
+		this.utilities = utilities
 
 		// Create save dir
-		const { directory: globalDirectory } = this.getGlobalConfigPath();
-		const { directory: localDirectory } = this.getLocalConfigPath();
+		const { directory: globalDirectory } = this.getGlobalConfigPath()
+		const { directory: localDirectory } = this.getLocalConfigPath()
 
 		if (!fs.existsSync(globalDirectory)) {
-			fs.mkdirSync(globalDirectory);
+			fs.mkdirSync(globalDirectory)
 		}
 
 		if (scope === StoreScope.Local && !fs.existsSync(localDirectory)) {
-			fs.mkdirSync(localDirectory);
+			fs.mkdirSync(localDirectory)
 		}
 	}
 	/** Write a value to disk (should only be used in save()) */
 	protected writeValue<F extends keyof Settings>(key: F, value: Settings[F]) {
-		this.writeValues({ [key]: value });
-		return;
+		this.writeValues({ [key]: value })
+		return
 	}
 
 	/** Write a whole object to disk (should only be used in save()) */
 	protected writeValues<T extends Record<string, any>>(values: T) {
-		const currentValues = this.readValues();
-		const updatedValues = { ...currentValues, ...values };
+		const currentValues = this.readValues()
+		const updatedValues = { ...currentValues, ...values }
 
 		const { file, directory } =
 			this.scope === StoreScope.Local
 				? this.getLocalConfigPath()
-				: this.getGlobalConfigPath();
+				: this.getGlobalConfigPath()
 
 		// Make sure dir exists
 		if (!fs.existsSync(directory)) {
-			fs.mkdirSync(directory);
+			fs.mkdirSync(directory)
 		}
 
-		const contents = JSON.stringify(updatedValues);
-		fs.writeFileSync(file, contents);
+		const contents = JSON.stringify(updatedValues)
+		fs.writeFileSync(file, contents)
 
-		return this;
+		return this
 	}
 
 	/** Read a single value */
 	protected readValue<F extends keyof Settings>(key: F) {
-		const settings = this.readValues();
-		return settings[key];
+		const settings = this.readValues()
+		return settings[key]
 	}
 
 	/** Read values from disk */
@@ -121,64 +121,64 @@ export default abstract class AbstractStore<
 		const { file, directory } =
 			this.scope === StoreScope.Local
 				? this.getLocalConfigPath()
-				: this.getGlobalConfigPath();
+				: this.getGlobalConfigPath()
 
 		try {
 			// Make sure dir exists
 			if (!fs.existsSync(directory)) {
-				fs.mkdirSync(directory);
+				fs.mkdirSync(directory)
 			}
 
-			const contents = fs.readFileSync(file, "utf8");
-			const values = JSON.parse(contents) as T;
-			return values;
+			const contents = fs.readFileSync(file, 'utf8')
+			const values = JSON.parse(contents) as T
+			return values
 		} catch (err) {
 			this.log.warn(
 				`AbstractStore.readValues failed to read settings file at ${file}`
-			);
+			)
 		}
 		// Falls back to an empty object
-		return {};
+		return {}
 	}
 
 	/** A copy of mercury authed against the token you sent */
 	protected async mercuryForUser(token: string): Promise<Mercury> {
-		const { connectionOptions } = this.mercury;
+		const { connectionOptions } = this.mercury
 		if (!connectionOptions) {
 			throw new SpruceError({
 				code: ErrorCode.GenericMercury,
 				friendlyMessage:
-					"user store was trying to auth on mercury but had no options (meaning it was never connected)"
-			});
+					'user store was trying to auth on mercury but had no options (meaning it was never connected)'
+			})
 		}
 		// Connect with new creds
 		await this.mercury.connect({
 			...(connectionOptions || {}),
 			credentials: { token }
-		});
+		})
 
-		return this.mercury;
+		return this.mercury
 	}
 
 	private getGlobalConfigPath() {
-		const homedir = require("os").homedir();
-		const configDirectory = `${homedir}/.spruce`;
-		const filePath = `${homedir}/.spruce/cli.json`;
+		const homedir = require('os').homedir()
+		const configDirectory = `${homedir}/.spruce`
+		const filePath = `${homedir}/.spruce/cli.json`
 
 		return {
 			directory: configDirectory,
 			file: filePath
-		};
+		}
 	}
 
 	private getLocalConfigPath() {
-		const homedir = this.cwd;
-		const configDirectory = `${homedir}/.spruce`;
-		const filePath = `${homedir}/.spruce/settings.json`;
+		const homedir = this.cwd
+		const configDirectory = `${homedir}/.spruce`
+		const filePath = `${homedir}/.spruce/settings.json`
 
 		return {
 			directory: configDirectory,
 			file: filePath
-		};
+		}
 	}
 }
