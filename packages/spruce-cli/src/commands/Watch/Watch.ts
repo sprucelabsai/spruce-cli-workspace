55fa0d36d80c42de9efe576d9b904106e68009b6
Ken Goldfarb <hello@kengoldfarb.com>
2020-04-10 11:26:56 -0400

    stub out watcher

--- packages/spruce-cli/src/commands/Watch/Watch.ts
+++ packages/spruce-cli/src/commands/Watch/Watch.ts
@@ -1,9 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Command } from 'commander'
+import chokidar, { FSWatcher } from 'chokidar'
 import AbstractCommand from '../AbstractCommand'
 import log from '../../lib/log'
 
+type CommandToExecute = string
+
+export interface IWatchConfig {
+	[globbyPattern: string]: CommandToExecute
+}
+
 export default class WatchCommand extends AbstractCommand {
+	private watcher!: FSWatcher
+	/**  */
+	// private resolve!: () => void
+	private reject!: (e: Error) => void
+
 	public attachCommands(program: Command): void {
 		// TODO: add option to override globby
 		// Or .autoloader
@@ -15,5 +27,32 @@ export default class WatchCommand extends AbstractCommand {
 
 	private async watch() {
 		log.debug(process.cwd())
+		this.watcher = chokidar.watch('**/*.ts')
+		this.watcher.on('change', this.onChange.bind(this))
+		this.watcher.on('add', this.onChange.bind(this))
+		this.watcher.on('error', this.onError.bind(this))
+
+		const finishedPromise = new Promise((resolve, reject) => {
+			// This.resolve = resolve
+			this.reject = reject
+		})
+		await finishedPromise
+	}
+
+	private onChange(path: string) {
+		log.debug(`${path} changed`)
+	}
+
+	private onError(e: Error) {
+		this.reject(e)
+	}
+
+	private getWatchConfig(): IWatchConfig {
+		// Default
+		const defaultConfig: IWatchConfig = {
+			'src/services/**/*Service.ts': 'autoloader -s Service ./src/services'
+		}
+
+		return defaultConfig
 	}
 }
