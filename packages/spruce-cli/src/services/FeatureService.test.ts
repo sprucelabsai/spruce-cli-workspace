e4db05cac433b497b7e02ceb4e8b1e1047f401b0
Ken Goldfarb <hello@kengoldfarb.com>
2020-05-06 12:00:35 -0400

    latest

--- packages/spruce-cli/src/services/FeatureService.test.ts
+++ packages/spruce-cli/src/services/FeatureService.test.ts
@@ -1,22 +1,24 @@
-import { test, assert } from '@sprucelabs/test'
-import fs from 'fs-extra'
-import uuid from 'uuid'
+import { assert } from '@sprucelabs/test'
+// import fs from 'fs-extra'
+// import uuid from 'uuid'
 import log from '../lib/log'
 import { setup } from '../index'
 import BaseTest from '../BaseTest'
 import { Feature } from '#spruce/autoloaders/features'
 import { IServices } from '#spruce/autoloaders/services'
 
-let services: IServices | undefined
+// let services: IServices | undefined
 
 export default class FeatureServiceTest extends BaseTest {
+	private static services: IServices | undefined
+
 	protected static async beforeAll() {
 		try {
 			const tmpDirectory = this.getTempDirectory()
 			const result = await setup({
 				cwd: tmpDirectory
 			})
-			services = result.services
+			this.services = result.services
 		} catch (e) {
 			log.crit(e)
 		}
@@ -24,7 +26,7 @@ export default class FeatureServiceTest extends BaseTest {
 
 	@test('Can get feature dependencies')
 	protected static async getFeatureDependencies() {
-		const dependencies = services?.feature.getFeatureDependencies({
+		const dependencies = this.services?.feature.getFeatureDependencies({
 			feature: Feature.Test
 		})
 		assert.isArray(dependencies)
@@ -34,32 +36,32 @@ export default class FeatureServiceTest extends BaseTest {
 		assert.equal(dependencies?.[2].feature, Feature.Test)
 	}
 
-	@test('Can get circular feature dependencies')
-	protected static async getCircularFeatureDependencies() {
-		const dependencies = services?.feature.getFeatureDependencies({
-			feature: Feature.Skill
-		})
-		assert.isArray(dependencies)
-		// Verify order
-		assert.equal(dependencies?.[0].feature, Feature.Skill)
-		assert.equal(dependencies?.[1].feature, Feature.Schema)
-		assert.equal(dependencies?.[2].feature, Feature.Test)
-	}
+	// @test('Can get circular feature dependencies')
+	// protected async getCircularFeatureDependencies() {
+	// 	const dependencies = services?.feature.getFeatureDependencies({
+	// 		feature: Feature.Skill
+	// 	})
+	// 	assert.isArray(dependencies)
+	// 	// Verify order
+	// 	assert.equal(dependencies?.[0].feature, Feature.Skill)
+	// 	assert.equal(dependencies?.[1].feature, Feature.Schema)
+	// 	assert.equal(dependencies?.[2].feature, Feature.Test)
+	// }
 
-	@test('Can install a feature')
-	protected static async installFeature() {
-		await services?.feature.install({
-			features: [
-				{
-					feature: Feature.Skill,
-					options: {
-						name: uuid.v4(),
-						description: uuid.v4()
-					}
-				}
-			]
-		})
-		console.log({ services })
-		assert.isTrue(fs.existsSync(services?.feature.cwd as string))
-	}
+	// @test('Can install a feature')
+	// protected async installFeature() {
+	// 	await services?.feature.install({
+	// 		features: [
+	// 			{
+	// 				feature: Feature.Skill,
+	// 				options: {
+	// 					name: uuid.v4(),
+	// 					description: uuid.v4()
+	// 				}
+	// 			}
+	// 		]
+	// 	})
+	// 	console.log({ services })
+	// 	assert.isTrue(fs.existsSync(services?.feature.cwd as string))
+	// }
 }
