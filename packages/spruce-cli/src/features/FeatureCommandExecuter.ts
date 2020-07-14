db56304e8120d0c6d6d37857b248c9914b75307d
liquidg3 <me@taylorrome.ro>
2020-07-14 11:13:44 -0600

    patch: NOW ready to run

--- packages/spruce-cli/src/features/FeatureCommandExecuter.ts
+++ packages/spruce-cli/src/features/FeatureCommandExecuter.ts
@@ -1,6 +1,8 @@
+import { features } from 'process'
 import {
 	ISchemaDefinition,
 	SchemaDefinitionPartialValues,
+	ISchema,
 } from '@sprucelabs/schema'
 import FormComponent from '../components/FormComponent'
 import TerminalInterface from '../interfaces/TerminalInterface'
@@ -34,34 +36,61 @@ export default class FeatureCommandExecuter<F extends FeatureCode> {
 
 	public async execute(options?: FeatureCommandExecuteOptions<F>) {
 		const feature = this.featureInstaller.getFeature(this.featureCode)
+		const action = feature.Action(this.actionCode)
 
 		if (feature.optionsDefinition) {
-			const featureForm = new FormComponent({
-				term: this.term,
-				definition: feature.optionsDefinition,
-				initialValues: options,
-			})
-
-			const fieldNames = Object.keys(feature.optionsDefinition.fields ?? {})
-			const providedFieldNames = options ? Object.keys(options ?? {}) : []
-			const fieldsToPresent = fieldNames.filter(
-				(name) => providedFieldNames.indexOf(name) === -1
+			const answers = await this.collectAnswers(
+				feature.optionsDefinition,
+				options
 			)
-			const answers = await featureForm.present({
-				showOverview: false,
-				// @ts-ignore
-				fields: fieldsToPresent,
-			})
 
-			await this.featureInstaller.install({
-				features: [
-					{
-						code: this.featureCode,
-						//@ts-ignore
-						options: { ...options, ...answers },
-					},
-				],
-			})
+			const isInstalled = await this.featureInstaller.isInstalled(
+				this.featureCode
+			)
+
+			if (!isInstalled) {
+				await this.featureInstaller.install({
+					features: [
+						{
+							code: this.featureCode,
+							//@ts-ignore
+							options: { ...options, ...answers },
+						},
+					],
+				})
+			}
 		}
+
+		if (action.optionsDefinition) {
+			const answers = await this.collectAnswers(
+				action.optionsDefinition,
+				options
+			)
+
+			await action.execute(answers)
+		}
+	}
+
+	private async collectAnswers(
+		definition: ISchemaDefinition,
+		options: FeatureCommandExecuteOptions<F> | undefined
+	) {
+		const featureForm = new FormComponent({
+			term: this.term,
+			definition,
+			initialValues: options,
+		})
+
+		const fieldNames = Object.keys(definition.fields ?? {})
+		const providedFieldNames = options ? Object.keys(options ?? {}) : []
+		const fieldsToPresent = fieldNames.filter(
+			(name) => providedFieldNames.indexOf(name) === -1
+		)
+		const answers = await featureForm.present({
+			showOverview: false,
+			// @ts-ignore
+			fields: fieldsToPresent,
+		})
+		return { ...(options ?? {}), ...answers }
 	}
 }
