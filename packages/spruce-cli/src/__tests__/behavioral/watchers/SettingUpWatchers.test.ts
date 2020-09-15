017e6fc0ea9307470edf87ca13bce4b5d7035dea
liquidg3 <me@taylorrome.ro>
2020-09-15 13:29:02 -0600

    patch: watch start/stop

--- packages/spruce-cli/src/__tests__/behavioral/watchers/SettingUpWatchers.test.ts
+++ packages/spruce-cli/src/__tests__/behavioral/watchers/SettingUpWatchers.test.ts
@@ -4,12 +4,45 @@ import AbstractCliTest from '../../../AbstractCliTest'
 export default class SettingUpWatchersTest extends AbstractCliTest {
 	@test()
 	protected static async installsWatchers() {
-		const fixture = this.FeatureFixture()
-
-		const cli = await fixture.installFeatures([{ code: 'watch' }])
-
+		const cli = await this.installWatch()
 		const isInstalled = await cli.getFeature('watch').isInstalled()
-
 		assert.isTrue(isInstalled)
+	}
+
+	@test()
+	protected static async notWatchingAtTheStart() {
+		const cli = await this.installWatch()
+		const isWatching = await cli.getFeature('watch').isWatching()
+		assert.isFalse(isWatching)
+	}
+
+	@test()
+	protected static async canStartAndStopWatch() {
+		const cli = await this.installWatch()
+		const feature = cli.getFeature('watch')
+
+		void feature.startWatching()
+
+		let isWatching = await feature.isWatching()
+		assert.isTrue(isWatching)
+
+		await feature.stopWatching()
+
+		isWatching = await feature.isWatching()
+		assert.isFalse(isWatching)
+	}
+
+	@test()
+	protected static async watcherFiresEventWhenASrcFileChanges() {
+		const cli = await this.installWatch()
+		const feature = cli.getFeature('watch')
+
+		void feature.startWatching()
+	}
+
+	private static async installWatch() {
+		const fixture = this.FeatureFixture()
+		const cli = await fixture.installFeatures([{ code: 'watch' }], 'watcher')
+		return cli
 	}
 }
