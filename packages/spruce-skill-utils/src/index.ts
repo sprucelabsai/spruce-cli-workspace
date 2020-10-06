97cc7f786c53e1e3296b4323970d4c391cc841f7
Ryan Johnson <ryan@sprucelabs.ai>
2020-10-06 15:58:24 -0400

    chore: remove spruce-skill-utils

--- packages/spruce-skill-utils/src/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as versionUtil } from './version.utility'
-export { default as diskUtil } from './disk.utility'
-export { default as namesUtil } from './names.utility'
-export { default as addonUtil } from './addon.utility'
-export { default as pluginUtil } from './plugin.utility'
-
-export { ErrorOptions as UtilErrorOptions } from './SpruceError'
-export * from './constants'
-export * from './global.types'
