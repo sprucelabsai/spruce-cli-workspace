190062f246387dd43bb147b6fd19ecf74afaa8ef
liquidg3 <me@taylorrome.ro>
2020-10-07 17:03:23 -0600

    patch: cleanup

--- packages/spruce-cli/src/.spruce/schemas/spruceCli/v2020_07_22/createSchemaAction.schema.ts
+++ packages/spruce-cli/src/.spruce/schemas/spruceCli/v2020_07_22/createSchemaAction.schema.ts
@@ -96,12 +96,12 @@ const createSchemaActionSchema: SpruceSchemas.SpruceCli.v2020_07_22.ICreateSchem
 	                defaultValue: true,
 	                options: undefined
 	            },
-	            /** Generate core schemas. Used only for updating the @sprucelabs/spruce-core-schemas. */
+	            /** Generate core schemas. Used only for updating the @sprucelabs/spruce-core-schemas. Alias for `--fetchCoreRemoteSchemas=false --fetchLocalSchemas=false --registerBuiltSchemas=false --generateStandaloneTypesFile. */
 	            'generateCoreSchemaTypes': {
 	                label: 'Generate core schemas',
 	                type: 'boolean',
 	                isPrivate: true,
-	                hint: 'Used only for updating the @sprucelabs/spruce-core-schemas.',
+	                hint: 'Used only for updating the @sprucelabs/spruce-core-schemas. Alias for `--fetchCoreRemoteSchemas=false --fetchLocalSchemas=false --registerBuiltSchemas=false --generateStandaloneTypesFile.',
 	                defaultValue: false,
 	                options: undefined
 	            },
