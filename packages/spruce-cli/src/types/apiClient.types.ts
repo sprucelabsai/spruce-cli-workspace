426d032f9e80fb699191808900aa203cab0fa978
Taylor Romero <me@taylorrome.ro>
2021-01-02 06:00:03 -0700

    patch: sync.ct back in, api client typed again

--- packages/spruce-cli/src/types/apiClient.types.ts
+++ packages/spruce-cli/src/types/apiClient.types.ts
@@ -1,11 +1,11 @@
 import { MercuryClient } from '@sprucelabs/mercury-client'
 import { SpruceSchemas } from '@sprucelabs/mercury-types'
-// import { EventContracts } from '#spruce/events/events.contract'
+import { EventContracts } from '#spruce/events/events.contract'
 
-export type ApiClientFactoryOptions = SpruceSchemas.MercuryApi.AuthenticateEmitPayload & {
+export type ApiClientFactoryOptions = SpruceSchemas.MercuryApi.v2020_12_25.AuthenticateEmitPayload & {
 	shouldAuthAsCurrentSkill?: boolean
 }
-export type ApiClient = MercuryClient<any>
+export type ApiClient = MercuryClient<EventContracts>
 export type ApiClientFactory = (
 	options?: ApiClientFactoryOptions
 ) => Promise<ApiClient>
