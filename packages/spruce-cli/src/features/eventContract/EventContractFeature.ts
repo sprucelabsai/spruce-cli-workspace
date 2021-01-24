f73e2a8d8c3983bd0ee6dbbba70f273dbc4ca405
Taylor Romero <me@taylorrome.ro>
2021-01-24 16:25:16 -0700

    patch: description update

--- packages/spruce-cli/src/features/eventContract/EventContractFeature.ts
+++ packages/spruce-cli/src/features/eventContract/EventContractFeature.ts
@@ -7,7 +7,7 @@ export default class EventContractFeature extends AbstractFeature {
 	public code: FeatureCode = 'eventContract'
 	public nameReadable = 'Event Contract'
 	public description =
-		'Pull core Mercury events down and write to single, portable, typed contract.'
+		'Pull core Mercury events down and write to single, portable, dependency-free, strongly typed contract.'
 	public dependencies: FeatureDependency[] = []
 	public packageDependencies = []
 
