2db94cbe73c55d0dcc322ee74d7c9253dc1163a2
Taylor Romero <me@taylorrome.ro>
2021-03-04 10:16:31 -0700

    no mroe logi

--- packages/spruce-templates/src/addons/fieldValue.addon.ts
+++ packages/spruce-templates/src/addons/fieldValue.addon.ts
@@ -1,14 +1,11 @@
 import handlebars from 'handlebars'
 import { FieldDefinitions } from '#spruce/schemas/fields/fields.types'
-import log from '../singletons/log'
 
 /** Drop in the value of a field which quotes if needed */
 handlebars.registerHelper(
 	'fieldValue',
 	function (fieldDefinitions: FieldDefinitions, value: any) {
 		if (value) {
-			// TODO finish this
-			log.debug(fieldDefinitions, value)
 			throw new Error('field value not yet implemented')
 		}
 
