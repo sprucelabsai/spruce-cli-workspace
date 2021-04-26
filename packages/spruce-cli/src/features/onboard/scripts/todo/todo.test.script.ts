5039562bbb2151cc805193420c5e559aa153fe88
Taylor Romero <me@taylorrome.ro>
2021-04-26 14:05:53 -0600

    link fix

--- packages/spruce-cli/src/features/onboard/scripts/todo/todo.test.script.ts
+++ packages/spruce-cli/src/features/onboard/scripts/todo/todo.test.script.ts
@@ -4,7 +4,7 @@ const script: Script = [
 	'Look at us go! 😄',
 	"I'm about to setup testing.",
 	'Real testing.',
-	'Remember the whole 3 Laws of Test Driven Development thing? You are def gonna wanna watch this before continuing -> https://bit.ly/3ls9phs',
+	'Remember the whole 3 Laws of Test Driven Development thing? You are def gonna wanna watch this before continuing -> https://youtu.be/qkblc5WRn-U',
 	'Ok, getting down off my robot soapbox.',
 	'First thing I gotta do is install some more dependencies.',
 	'You read to go?',
