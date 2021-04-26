d33624a49ee5ebf09d2419bd0af82286950f55e6
Taylor Romero <me@taylorrome.ro>
2021-04-26 14:07:34 -0600

    spelling fixes

--- packages/spruce-cli/src/features/onboard/scripts/todo/todo.test.script.ts
+++ packages/spruce-cli/src/features/onboard/scripts/todo/todo.test.script.ts
@@ -6,8 +6,8 @@ const script: Script = [
 	'Real testing.',
 	'Remember the whole 3 Laws of Test Driven Development thing? You are def gonna wanna watch this before continuing -> https://youtu.be/qkblc5WRn-U',
 	'Ok, getting down off my robot soapbox.',
-	'First thing I gotta do is install some more dependencies.',
-	'You read to go?',
+	'Next thing I gotta do is install some more dependencies (I know right?).',
+	'You ready to go?',
 	async (player) => {
 		await player.ui.waitForEnter('')
 	},
