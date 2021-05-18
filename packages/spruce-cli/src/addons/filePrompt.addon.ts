17f89fd83f176f1a55ae802f9895361c9d63ade9
Taylor Romero <me@taylorrome.ro>
2021-05-17 18:30:40 -0600

    patch: lint again

--- packages/spruce-cli/src/addons/filePrompt.addon.ts
+++ packages/spruce-cli/src/addons/filePrompt.addon.ts
@@ -1,5 +1,5 @@
-// @ts-ignore No definition available
 import inquirer from 'inquirer'
+// @ts-ignore No definition available
 import fileSelect from 'inquirer-file-tree-selection-prompt'
 
 inquirer.registerPrompt('file', fileSelect)
