f915bd32ed1036e3fee8cbbd158dff473d3ef462
Taylor Romero <me@taylorrome.ro>
2021-06-02 17:47:50 -0600

    remove debugger

--- packages/spruce-templates/src/addons/valueTypeMapper.addon.ts
+++ packages/spruce-templates/src/addons/valueTypeMapper.addon.ts
@@ -3,7 +3,6 @@ import handlebars from 'handlebars'
 handlebars.registerHelper(
 	'valueTypeMapper',
 	function (typeMapper: string, renderAs: 'typeOnly' | 'asGeneric') {
-		debugger
 		if (renderAs === 'typeOnly') {
 			return typeMapper.split('<')[0]
 		} else {
