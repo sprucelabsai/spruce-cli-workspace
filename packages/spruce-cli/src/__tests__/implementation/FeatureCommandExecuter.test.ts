ab9b5e8197323969a3bd26f7cf7692753bbc871a
Tay <me@taylorrome.ro>
2021-06-14 11:37:04 -0600

    patch: split up executer tests

--- packages/spruce-cli/src/__tests__/implementation/FeatureCommandExecuter.test.ts
+++ packages/spruce-cli/src/__tests__/implementation/FeatureCommandExecuter.test.ts
@@ -7,13 +7,8 @@ import {
 import { test, assert } from '@sprucelabs/test'
 import { errorAssertUtil } from '@sprucelabs/test-utils'
 import featuresUtil from '../../features/feature.utilities'
-import {
-	FeatureActionResponse,
-	FeatureCode,
-	FeatureInstallResponse,
-} from '../../features/features.types'
+import { FeatureCode } from '../../features/features.types'
 import AbstractSchemaTest from '../../tests/AbstractSchemaTest'
-import testUtil from '../../tests/utilities/test.utility'
 
 export default class FeatureCommandExecuterTest extends AbstractSchemaTest {
 	@test()
@@ -126,48 +121,6 @@ export default class FeatureCommandExecuterTest extends AbstractSchemaTest {
 	}
 
 	@test()
-	protected static async shouldAddListenerWithoutBreakingOnSkill() {
-		await this.FeatureFixture().installCachedFeatures('schemas')
-
-		const executer = this.Action('event', 'listen', {
-			shouldAutoHandleDependencies: true,
-		})
-		const promise = executer.execute({})
-
-		await this.waitForInput()
-
-		await this.ui.sendInput('skill')
-		await this.ui.sendInput('will-boot')
-
-		const results = await promise
-
-		const version = versionUtil.generateVersion().dirValue
-		testUtil.assertFileByNameInGeneratedFiles(
-			`will-boot.${version}.listener.ts`,
-			results.files
-		)
-	}
-
-	@test()
-	protected static async shouldAskInstallDependentFeatures() {
-		const executer = this.Action('schema', 'create', {
-			shouldAutoHandleDependencies: true,
-		})
-		void executer.execute({})
-
-		await this.waitForInput()
-
-		this.ui.reset()
-		const lastQuestion = this.ui.lastInvocation()
-
-		assert.isEqual(lastQuestion.command, 'prompt')
-		assert.doesInclude(
-			lastQuestion.options.label,
-			/install the skill feature/gi
-		)
-	}
-
-	@test()
 	protected static async shouldInstallDependentFeatures() {
 		const executer = this.Action('schema', 'create', {
 			shouldAutoHandleDependencies: true,
@@ -267,42 +220,6 @@ export default class FeatureCommandExecuterTest extends AbstractSchemaTest {
 		assert.isTrue(isInstalled)
 	}
 
-	@test()
-	protected static async shouldReturnProperSummary() {
-		const executer = this.Action('skill', 'create', {
-			shouldAutoHandleDependencies: true,
-		})
-		const results = await executer.execute({
-			name: 'install summary test skill',
-			description: 'go team!',
-		})
-
-		assert.isTruthy(results.files)
-		assert.isTruthy(results.packagesInstalled)
-
-		assert.isAbove(results.files.length, 0)
-		assert.isAbove(results.packagesInstalled.length, 0)
-	}
-
-	@test()
-	protected static async canSkipOptionalDependencies() {
-		const { promise: actionPromise } =
-			await this.startBuildingNewErrorUntilOptionalDependencies('skip')
-
-		const results = await this.finishBuildingUpToNamingNewError(actionPromise)
-
-		assert.isTruthy(results.files)
-		assert.isFalsy(results.errors)
-
-		const pluginsDir = this.resolveHashSprucePath('features')
-		assert.isFalse(diskUtil.doesDirExist(pluginsDir))
-
-		testUtil.assertFileByNameInGeneratedFiles(
-			'myNewError.schema.ts',
-			results.files
-		)
-	}
-
 	private static async assertHealthySkillNamed(
 		name: string,
 		expectedHealth: HealthCheckResults = { skill: { status: 'passed' } },
@@ -330,111 +247,5 @@ export default class FeatureCommandExecuterTest extends AbstractSchemaTest {
 			const isInstalled = await installer.isInstalled(code)
 			assert.isTrue(isInstalled)
 		}
-	}
-
-	private static async finishBuildingUpToNamingNewError(
-		actionPromise: Promise<FeatureInstallResponse & FeatureActionResponse>
-	) {
-		await this.ui.sendInput('')
-
-		await this.waitForInput()
-
-		await this.ui.sendInput('')
-
-		await this.waitForInput()
-
-		await this.ui.sendInput('My new error')
-		await this.ui.sendInput('')
-
-		const results = await actionPromise
-		return results
-	}
-
-	private static async startBuildingNewErrorUntilOptionalDependencies(
-		skillFeatureAnswer: 'skip' | 'yes' | 'alwaysSkip'
-	) {
-		const executer = this.Action('error', 'create', {
-			shouldAutoHandleDependencies: true,
-		})
-		const promise = executer.execute({})
-
-		await this.waitForInput()
-
-		const message =
-			this.ui.invocations[this.ui.invocations.length - 2].options.message
-		assert.isTruthy(message)
-
-		assert.doesInclude(message, /2 required/gi)
-		assert.doesInclude(message, /1 optional/gi)
-
-		assert.doesInclude(this.ui.lastInvocation(), {
-			command: 'prompt',
-			options: {
-				type: 'select',
-				options: {
-					choices: [
-						{
-							value: 'yes',
-							label: 'Yes',
-						},
-						{
-							value: 'skip',
-							label: 'Skip',
-						},
-						{
-							value: 'alwaysSkip',
-							label: 'Always skip',
-						},
-					],
-				},
-			},
-		})
-
-		await this.ui.sendInput(skillFeatureAnswer)
-
-		await this.ui.sendInput('y')
-
-		await this.waitForInput()
-
-		await this.ui.sendInput('My new module')
-		await this.ui.sendInput('it is for testing')
-
-		await this.waitForInput()
-
-		return { promise }
-	}
-
-	@test()
-	protected static async canPermanentlySkipOptionalDependencies() {
-		const { promise: actionPromise } =
-			await this.startBuildingNewErrorUntilOptionalDependencies('alwaysSkip')
-
-		await this.finishBuildingUpToNamingNewError(actionPromise)
-
-		const executer = this.Action('error', 'create', {
-			shouldAutoHandleDependencies: true,
-		})
-		const promise = executer.execute({})
-
-		await this.waitForInput()
-
-		assert.doesNotInclude(this.ui.lastInvocation(), {
-			command: 'prompt',
-			options: {
-				type: 'select',
-			},
-		})
-
-		await this.ui.sendInput('My second error')
-		await this.ui.sendInput('')
-
-		const secondTimeResults = await promise
-
-		assert.isTruthy(secondTimeResults.files)
-
-		testUtil.assertFileByNameInGeneratedFiles(
-			'myNewError.schema.ts',
-			secondTimeResults.files
-		)
 	}
 }
