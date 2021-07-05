7503137dfe90fcd971880082ec95b2b442d673ea
Tay <me@taylorrome.ro>
2021-07-05 16:42:21 -0600

    patch: test

--- packages/spruce-cli/src/services/PkgService.ts
+++ packages/spruce-cli/src/services/PkgService.ts
@@ -115,7 +115,7 @@ export default class PkgService extends CommandService {
 			!diskUtil.doesDirExist(pathUtil.join(this.cwd, 'node_modules')) ||
 			labsModules.length > 0
 		) {
-			await this.execute('npm')
+			await this.execute('npm install')
 		}
 
 		this.deleteLockFile()
